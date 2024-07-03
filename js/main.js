23ccbe7f590a1dc6ca6796b7b9d9a78f1b81fa29
herrera22 <herreraabel@gmail.com>
2024-07-03 20:44:27 -0300

    pantalla de incio terminada

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 let valorCorrecto = '5555'
+let card = document.getElementById("cardMedico")
 
- const test=()=>{
-
+const test=()=>{
+    let codigoCorrecto = document.getElementById("codigo").value
+    let nombrePaciente = document.getElementById("nombre").value
+    console.log(codigoCorrecto)
+    console.log(nombrePaciente)
+    if(codigoCorrecto === valorCorrecto){
+        card.style.display="none"
+    }
  }
