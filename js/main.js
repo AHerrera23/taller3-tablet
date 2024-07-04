a23d5dc5b9828dbb2cd3b2515e85574dbdfe2833
herrera22 <herreraabel@gmail.com>
2024-07-04 06:47:59 -0300

    commit casi final

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 let valorCorrecto = '5555'
+let idSofi= '817475'
+let idJuli= '913911'
+let test1 = document.getElementById('test')
 let card = document.getElementById("cardMedico")
 
 const test=()=>{
     let codigoCorrecto = document.getElementById("codigo").value
     let nombrePaciente = document.getElementById("nombre").value
+    let idMedico = document.getElementById("idMedico").value
     console.log(codigoCorrecto)
     console.log(nombrePaciente)
     if(codigoCorrecto === valorCorrecto){
         card.style.display="none"
+        if(idMedico === idSofi){
+            test1.innerHTML='Bienvenida Dra Mofia'
+        }
+        else if(idMedico === idJuli){
+            test1.innerHTML='Bienvenida Dra Alegria'
+        }
     }
  }
