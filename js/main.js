75429bcaef8066e98045d7c428ad71fbde89bb67
herrera22 <herreraabel@gmail.com>
2024-07-04 07:12:36 -0300

    ultimos commits

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-let valorCorrecto = '5555'
+let valorCorrecto = '780201'
 let idSofi= '817475'
 let idJuli= '913911'
 let test1 = document.getElementById('test')
 let card = document.getElementById("cardMedico")
+let card2 = document.getElementById("cardMedica2")
 
 const test=()=>{
     let codigoCorrecto = document.getElementById("codigo").value
@@ -12,11 +13,12 @@ const test=()=>{
     console.log(nombrePaciente)
     if(codigoCorrecto === valorCorrecto){
         card.style.display="none"
+        card2.style.display="flex"
         if(idMedico === idSofi){
-            test1.innerHTML='Bienvenida Dra Mofia'
+            test1.innerHTML='Bienvenida Dra Sofia'
         }
         else if(idMedico === idJuli){
-            test1.innerHTML='Bienvenida Dra Alegria'
+            test1.innerHTML='Bienvenida Dra Julia'
         }
     }
  }
